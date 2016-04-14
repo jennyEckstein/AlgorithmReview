e7b3d934ad1225c477340c6bc406fe6b93314bdb
Jenny <ycharnetskaya@gmail.com>
2016-04-13 23:41:03 -0400

    Bubble Sort Algorithm added
    Bubble sort algorithm is added and tested.

--- src/sortingAlgorithms/BubbleSortAlgorithm.java
+++ src/sortingAlgorithms/BubbleSortAlgorithm.java
@@ -1,37 +1,49 @@
 package sortingAlgorithms;
 
 public class BubbleSortAlgorithm {
-	
-	private int [] arrayToSort; 
 
-	public BubbleSortAlgorithm(int [] array){
+	private int[] arrayToSort;
+
+	public BubbleSortAlgorithm(int[] array) {
 		this.arrayToSort = array;
 	}
-	
-	public int [] bubbleSort(){
-		
-		return null;
-	}
-	
-	public static void main(String[] args) {
-		
-		int [] unsortedArray = {9,8,7,6,5,4,3,2,1};
-		
-		BubbleSortAlgorithm useBubbleSort = new BubbleSortAlgorithm(unsortedArray);
-		int [] sortedArray = useBubbleSort.bubbleSort();
-		
-		if (sortedArray != null){
-			System.out.println("Unsorted Array");
-			for (int unsortedValue: unsortedArray){
-				System.out.print(unsortedValue + ", ");
+
+	public int[] bubbleSort() {
+
+		int temp;
+		boolean sorted = false;
+		while (!sorted) {
+			sorted = true;
+			for (int i = 0; i < arrayToSort.length - 1; i++) {
+				if (arrayToSort[i] > arrayToSort[i + 1]) {
+					temp = arrayToSort[i + 1];
+					arrayToSort[i + 1] = arrayToSort[i];
+					arrayToSort[i] = temp;
+					sorted = false;
+				}
 			}
-			
-			System.out.println("Sorted Array");
-			for (int sortedValue: sortedArray){
+		}
+		return arrayToSort;
+	}
+
+	public static void main(String[] args) {
+
+		int[] unsortedArray = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+		BubbleSortAlgorithm useBubbleSort = 
+				new BubbleSortAlgorithm(unsortedArray.clone());
+		int[] sortedArray = useBubbleSort.bubbleSort();
+		System.out.println("Unsorted Array");
+		for (int unsortedValue : unsortedArray) {
+			System.out.print(unsortedValue + ", ");
+		}
+		if (sortedArray != null) {
+			System.out.println("\nSorted Array");
+			for (int sortedValue : sortedArray) {
 				System.out.print(sortedValue + ", ");
 			}
-		}else{
-			System.out.println ("Sorted array is null");
+		} else {
+			System.out.println("Sorted array is null");
 		}
 
 	}
