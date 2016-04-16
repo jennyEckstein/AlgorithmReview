207a67018b1895a78e9197388b88c1bec58afe68
Jenny <ycharnetskaya@gmail.com>
2016-04-16 11:27:40 -0400

    Added Selection Sort algorithm
    Added and tested algorithm

--- src/sortingAlgorithms/SelectionSortAlgorithm.java
+++ src/sortingAlgorithms/SelectionSortAlgorithm.java
@@ -10,13 +10,31 @@ public class SelectionSortAlgorithm {
 
 	public int[] selectionSort() {
 	
+		for (int i = 0; i < arrayToSort.length-1; i++){
+			int currentMin = arrayToSort[i];
+			
+			int currentSwapPosition = -1;
+			for(int j = i+1; j < arrayToSort.length; j++){
+				if (currentMin > arrayToSort[j]){
+					currentMin = arrayToSort[j];
+					currentSwapPosition = j;
+				}
+			}
+			if (currentSwapPosition != -1){
+			    arrayToSort[currentSwapPosition] = arrayToSort[i];
+			    arrayToSort[i] = currentMin;
+			}
+			
+			
+		}
 		return null;
 	}
 
 	public static void main(String[] args) {
-
-		int[] unsortedArray = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-
+		System.out.println("Selection Sort Algorithm");
+		//int[] unsortedArray = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		int[] unsortedArray = {23,42,4,16,8,15};
+		
 		BubbleSortAlgorithm useBubbleSort = 
 				new BubbleSortAlgorithm(unsortedArray.clone());
 		int[] sortedArray = useBubbleSort.bubbleSort();
