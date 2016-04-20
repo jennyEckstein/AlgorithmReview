651a3462b4956efea534d5369dcac6b938f52589
Jenny <ycharnetskaya@gmail.com>
2016-04-19 21:24:14 -0400

    feat: implemented MergeSort Algorithm

--- src/sortingAlgorithms/MergeSortAlgorithm.java
+++ src/sortingAlgorithms/MergeSortAlgorithm.java
@@ -2,36 +2,61 @@ package sortingAlgorithms;
 
 public class MergeSortAlgorithm {
 
-	private int[] arrayToSort;
-
-	public MergeSortAlgorithm(int[] array) {
-		this.arrayToSort = array;
+	public void mergeSort(int[] array, int low, int high) {
+		int size = array.length;
+		if(low < high){
+			int middle = (low + high) / 2;
+			mergeSort(array, low, middle);
+			mergeSort(array, middle+1, high);
+			merge(array, low,middle, high);
+		}
+		
+		//return arrayToSort;
 	}
-
-	public int[] mergeSort() {
 	
-		return arrayToSort;
+	private void merge(int [] array, int low, int middle, int high){
+		int size = array.length;
+		int[] temp = new int[size];
+		for (int i = low; i <= high; ++i){
+			temp[i] = array[i];
+		}
+		int i = low;
+		int j  = middle + 1;
+		int k = low;
+		
+		while (i <= middle && j <= high){
+			if (temp[i] <= temp[j]){
+				array[k] = temp[i];
+				++i;
+			}else{
+				array[k] = temp[j];
+				++j;
+			}
+			++k;
+		}
+		
+		while (i <= middle){
+			array[k] = temp[i];
+			++k;
+			++i;
+		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Selection Sort Algorithm");
 		//int[] unsortedArray = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 		int[] unsortedArray = {23,42,4,16,8,15};
-		
-		MergeSortAlgorithm useBubbleSort = 
-				new MergeSortAlgorithm(unsortedArray.clone());
-		int[] sortedArray = useBubbleSort.mergeSort();
 		System.out.println("Unsorted Array");
 		for (int unsortedValue : unsortedArray) {
 			System.out.print(unsortedValue + ", ");
 		}
-		if (sortedArray != null) {
-			System.out.println("\nSorted Array");
-			for (int sortedValue : sortedArray) {
-				System.out.print(sortedValue + ", ");
-			}
-		} else {
-			System.out.println("Sorted array is null");
+		MergeSortAlgorithm useBubbleSort = 
+				new MergeSortAlgorithm();
+		useBubbleSort.mergeSort(unsortedArray, 0, unsortedArray.length-1);
+		
+		System.out.println("\nSorted Array");
+		for (int sortedValue : unsortedArray) {
+			System.out.print(sortedValue + ", ");
 		}
 
 	}
