a0b2977edbf0f1bb414e11cd4a7cbae60f9f0219
Jenny <ycharnetskaya@gmail.com>
2016-04-21 19:42:18 -0400

    feat: added inOrderTraversal method for binary tree
    Inside the inOrderTraversal method, there is a system print out that
    shows how elements line up in order

--- src/binaryTree/BinaryTree.java
+++ src/binaryTree/BinaryTree.java
@@ -2,4 +2,54 @@ package binaryTree;
 
 public class BinaryTree {
 
+	Node root;
+	
+	public void addNode(int key, String name){
+		Node newNode = new Node(key, name);
+		
+		if (root == null){
+			this.root = newNode;
+		}else{
+			Node focusNode = root;
+			Node parent;
+			
+			while (true){
+				parent = focusNode;
+				if(key < focusNode.key){
+					focusNode =  focusNode.leftChild;
+					if(focusNode == null){
+						parent.leftChild = newNode;
+						return;
+					}
+				}else{
+					focusNode = focusNode.rightChild;
+					if(focusNode == null){
+						parent.rightChild = newNode;
+						return;
+					}
+				}
+			}
+		}
+	}
+	
+	public void inOrderTraverseTree(Node focusNode){
+		if (focusNode != null){
+			inOrderTraverseTree(focusNode.leftChild);
+			System.out.println(focusNode.toSting());
+			inOrderTraverseTree(focusNode.rightChild);
+		}
+	}
+	
+	public static void main (String [] args){
+		BinaryTree tree = new BinaryTree();
+		tree.addNode(50, "A");
+		tree.addNode(25, "B");
+		tree.addNode(15, "C");
+		tree.addNode(30, "D");
+		tree.addNode(75, "E");
+		tree.addNode(85, "F");
+		
+		tree.inOrderTraverseTree(tree.root);
+		
+	}
 }
--- src/binaryTree/Node.java
+++ src/binaryTree/Node.java
@@ -1,5 +1,18 @@
 package binaryTree;
 
 public class Node {
-
+	int key;
+	String name;
+	
+	Node leftChild;
+	Node rightChild;
+	
+	public Node(int key, String name){
+		this.key = key;
+		this.name = name;
+	}
+	
+	public String toSting(){
+		return name + " has a key " + key;
+	}
 }
